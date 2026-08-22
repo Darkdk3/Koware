157ed7f671f1aaefcfbbab215b6865407f5743c4
Darkdk3 <125339350+Darkdk3@users.noreply.github.com>
2026-08-22 17:24:36 +0200

    Fix import statement for Row in DiscoverSourcePickerDialog

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/discover/DiscoverSourcePickerDialog.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/discover/DiscoverSourcePickerDialog.kt
@@ -2,13 +2,13 @@
 
 package eu.kanade.tachiyomi.ui.browse.discover
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Row
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
